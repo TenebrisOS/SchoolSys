f086606d35e0a728fc1229cf9c2e3bae1a8404d8
Tenebris <naniiloveminecraft@outlook.com>
2023-07-29 07:33:47 -0700

    aa

--- main.py
+++ main.py
@@ -1,46 +1,8 @@
 import customtkinter as ctk
 import json
 
-
-def final_grade(firstname, lastname):
-    with open('files/' + firstname.lower() + '_' + lastname.lower() + '.json') as f:
-        data = json.load(f)
-
-    # grades
-    subjects = data['SUBJECTS']
-    maths = data['MATHS']
-    pc = data['PC']
-    english = data['ENGLISH']
-    science = data['SCIENCE']
-    i = (maths + pc + english + science)//subjects
-    return i
-
-
-def give_grade(grade):
-    if grade < 10:
-        return "F"
-    elif grade < 12 and grade > 10:
-        return 'D'
-    elif grade < 14 and grade > 12:
-        return 'C'
-    elif grade < 16 and grade > 14:
-        return 'B-'
-    elif grade < 18 and grade > 16:
-        return 'B+'
-    elif grade < 20 and grade > 18:
-        return 'A'
-    elif grade == 20:
-        return "A+"
-    else:
-        return
-
-
-def get_class(firstname, lastname):
-    with open('files/' + firstname.lower() + '_' + lastname.lower() + '.json') as f:
-        data = json.load(f)
-    classs = data["CLASS"]
-    return classs
-
+with open('files/student_data.json') as f:
+    data = json.load(f)
 
 root = ctk.CTk()
 root.geometry('640x480')
@@ -54,28 +16,41 @@ label.place(x=20, y=25)
 # label.pack()
 
 
-def draw_report(grade):
+def draw_report(grade, age, classes, id):
     # progressbar = ctk.CTkProgressBar(root, orientation="horizontal")
     # progressbar.configure(mode="indeterminate", variable= grade)
+    agectk = ctk.CTkLabel(root, text=(
+        "Age : " + str(age)), fg_color="transparent", font=('arial', 20))
+    agectk.place(x=20, y=150)
+    classctk = ctk.CTkLabel(root, text=(
+        "Class : " + str(classes)), fg_color="transparent", font=('arial', 20))
+    classctk.place(x=20, y=200)
+    idctk = ctk.CTkLabel(root, text=(
+        "ID : " + str(id)), fg_color="transparent", font=('arial', 20))
+    idctk.place(x=20, y=250)
     finalgrade = ctk.CTkLabel(root, text=(
-        "Final Grade : " + grade), fg_color="transparent", font=('arial', 20))
+        "Final Grade : " + str(grade)), fg_color="transparent", font=('arial', 20))
     finalgrade.place(x=20, y=100)
+    if grade > 10:
+        label = ctk.CTkLabel(root, text=("Admitted !!"),
+                             text_color="green", font=('arial', 40))
+        label.place(x=200, y=90)
+
 
 def on_enter(event):
-    entry.delete(0, ctk.END)
     name = entry.get()
-    grp = str(name).split("\n")
-    if len(grp) == 2:
-        print("noir")
-        firstname = grp[0]
-        lastname = grp[1]
-        finalgrade = final_grade(firstname, lastname)
-        grade = give_grade(finalgrade)
-        classs = get_class(firstname, lastname)
-        draw_report(grade=finalgrade)
+    entry.delete(0, ctk.END)
+    print(name)
+    correctedName = name.title()
+    studentdata = data[correctedName]
+    id = studentdata["id"]
+    classes = studentdata['class']
+    age = studentdata['age']
+    grade = studentdata['finalgrade']
+    draw_report(grade=grade, age=age, classes=classes, id=id)
 
 
-entry = ctk.CTkEntry(master=root, width=200, font=("arial", 10))
+entry = ctk.CTkEntry(master=root, width=200, font=("arial", 15))
 entry.place(x=20, y=50)
 entry.bind("<Return>", on_enter)
 
