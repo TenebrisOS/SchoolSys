bd9318432ae74f7eef4d0e327f8fef3318ead665
Tenebris <naniiloveminecraft@outlook.com>
2023-07-29 07:56:11 -0700

    no changes

--- main.py
+++ main.py
@@ -17,24 +17,32 @@ label.place(x=20, y=25)
 
 
 def draw_report(grade, age, classes, id):
+    reportcanvas = ctk.CTkCanvas(root, width=640, height=480)
+    reportcanvas.configure(bg='black')
     # progressbar = ctk.CTkProgressBar(root, orientation="horizontal")
     # progressbar.configure(mode="indeterminate", variable= grade)
-    agectk = ctk.CTkLabel(root, text=(
+    agectk = ctk.CTkLabel(reportcanvas, text=(
         "Age : " + str(age)), fg_color="transparent", font=('arial', 20))
     agectk.place(x=20, y=150)
-    classctk = ctk.CTkLabel(root, text=(
+    classctk = ctk.CTkLabel(reportcanvas, text=(
         "Class : " + str(classes)), fg_color="transparent", font=('arial', 20))
-    classctk.place(x=20, y=200)
-    idctk = ctk.CTkLabel(root, text=(
+    classctk.place(x=20, y=175)
+    idctk = ctk.CTkLabel(reportcanvas, text=(
         "ID : " + str(id)), fg_color="transparent", font=('arial', 20))
-    idctk.place(x=20, y=250)
-    finalgrade = ctk.CTkLabel(root, text=(
+    idctk.place(x=20, y=200)
+    finalgrade = ctk.CTkLabel(reportcanvas, text=(
         "Final Grade : " + str(grade)), fg_color="transparent", font=('arial', 20))
-    finalgrade.place(x=20, y=100)
+    finalgrade.place(x=20, y=225)
     if grade > 10:
-        label = ctk.CTkLabel(root, text=("Admitted !!"),
+        label = ctk.CTkLabel(reportcanvas, text=("Admitted !!"),
                              text_color="green", font=('arial', 40))
         label.place(x=200, y=90)
+    if grade < 10:
+        label = ctk.CTkLabel(reportcanvas, text=("Not admitted :("),
+                             text_color="red", font=('arial', 40))
+        label.place(x=200, y=90)
+    reportcanvas.pack()
+
 
 
 def on_enter(event):
